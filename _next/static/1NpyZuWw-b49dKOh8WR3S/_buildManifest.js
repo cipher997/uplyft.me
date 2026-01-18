e4a4f657c988c09c64fc0b3ad7fed850b3b89fce
cipher997 <kartik97gaikwad@gmail.com>
2026-01-18 16:35:39 +0530

    Updates

--- _next/static/1NpyZuWw-b49dKOh8WR3S/_buildManifest.js
+++ _next/static/1NpyZuWw-b49dKOh8WR3S/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/uplyft.me//_next/:path+",
+        "destination": "/uplyft.me/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
