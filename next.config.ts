0f969ba113a522f0da2f8b6092fa181a8db39cff
cipher997 <kartik97gaikwad@gmail.com>
2026-01-19 19:54:17 +0530

    update next.config.ts and Hero.tsx

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output:"export",
-  basePath: "/uplyft.me",
-  assetPrefix: "/uplyft.me/",
 };
 
 export default nextConfig;
